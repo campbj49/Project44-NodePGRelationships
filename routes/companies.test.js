8bc2bd9e3e5455d26675dc18bf6939a9e5d6f2e9
John Campbell <campbj49@gmail.com>
2023-08-31 16:52:08 -0400

    get test files to actually work

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -1,11 +1,11 @@
 /**Test company routes modified from example code in "Intro to Postress with Node" */
 /** GET /companies - returns `{companies: [company, ...]}` */
 const app = require("../app.js");
-const axios = require("axios");
+const request = require("supertest");
 
 describe("GET /companies", function() {
     test("Gets a list of 2 companies", async function() {
-      const response = await axios.request(app).get(`/companies`);
+      const response = await request(app).get(`/companies`);
       expect(response.statusCode).toEqual(200);
       expect(response.body).toEqual({
             companies:[{"code":"apple","name":"Apple Computer"},{"code":"ibm","name":"IBM"}]
